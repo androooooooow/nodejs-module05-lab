64bd114949e777dab0e189af0b624dded7acc6a0
jhooniee <jopa.prestoza.up@phinmaed.com>
2026-08-15 17:38:34 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   console.log(`Request received: ${req.url}`);
+    setTimeout(() => {
 
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-
-  res.end('Hello World from Node.js!');
+  res.end('Response after 2 seconds');
+    console.log(`Response sent: ${req.url}`);
+}, 2000);
 });
-
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
